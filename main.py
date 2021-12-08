a6824b65d3a77f88b58602634028c30e942c370e
LITARcrash <toprobocopid@gmail.com>
2021-12-08 21:54:34 +0300

    new bugfix

--- main.py
+++ main.py
@@ -43,86 +43,96 @@ def su_us(id):
 
 
         if last_name.lower() in putan_last_name_variations:
-            putan_rating = putan_rating + 0.96
+            putan_rating = putan_rating + 0.86
             options = options + 1
         for l_n_test in last_name:
 
             if l_n_test in putan_name_key_words:
-                putan_rating = putan_rating + 0.84
+                putan_rating = putan_rating + 0.74
                 options = options + 1
  
 
     if selected_user_main_info['no_wall_replies'] == 1:
-        putan_rating = putan_rating + 1.2
+        putan_rating = putan_rating + 0.97
         options = options + 1
 
-
+    print(len(selected_user_status['text']))
+    if 10 > len(selected_user_status['text']) >= 5:
+        putan_rating = putan_rating + 0.1
+        options = options + 1
+    if 15 > len(selected_user_status['text']) >= 10:
+        putan_rating = putan_rating + 0.2
+        options = options + 1
+    if 20 > len(selected_user_status['text']) >= 15:
+        putan_rating = putan_rating + 0.3
+        options = options + 1
+    if 70 >= len(selected_user_status['text']) >= 20:
+        putan_rating = putan_rating + 0.4
+        options = options + 1
 
     for selected_user_status_s in selected_user_status['text']:
         selected_user_status_symbols = len(selected_user_status_s)
-        if len(selected_user_status['text']) >= 5:
-            putan_rating = putan_rating + 0.004
-            options = options + 1
-        if 15 > len(selected_user_status['text']) >= 10:
-            putan_rating = putan_rating + 0.004
-            options = options + 1
-        if 20 > len(selected_user_status['text']) >= 15:
-            putan_rating = putan_rating + 0.004
-            options = options + 1
-        if len(selected_user_status['text']) >= 20:
-            putan_rating = putan_rating + 0.004
-            options = options + 1
+
+
         if selected_user_status_s in putan_status:
 
             if selected_user_status_symbols == 1 or 2:
-                putan_rating = putan_rating + 0.76
+                putan_rating = putan_rating + 0.56
                 options = options + 1
+
             if selected_user_status_symbols >= 3:
-                putan_rating = putan_rating + 0.94
+                putan_rating = putan_rating + 0.74
                 options = options + 1
+
+
         if selected_user_status_s in putan_status_dates:
             if selected_user_status_symbols > 1:
-                putan_rating = putan_rating + 0.78
+                putan_rating = putan_rating + 0.58
                 options = options + 1
+
         if selected_user_status_s in putan_status_key_words:
 
             if selected_user_status_symbols == 1 or 2:
                 putan_rating = putan_rating + 0.32
                 options = options + 1
 
+
             if 2 < selected_user_status_symbols <= 4:
                 putan_rating = putan_rating + 0.56
                 options = options + 1
 
 
+
     if 100 >= len(selected_user_friends['items']) > 50:
-        putan_rating = putan_rating + 0.64
+        putan_rating = putan_rating + 0.44
         options = options + 1
+
+
 
     if 150 >= len(selected_user_friends['items']) > 100:
 
-        putan_rating = putan_rating + 1.12
+        putan_rating = putan_rating + 0.55
         options = options + 1
 
     if 200 >= len(selected_user_friends['items']) > 150:
 
-        putan_rating = putan_rating + 1.08
+        putan_rating = putan_rating + 0.75
         options = options + 1
     if 300 >= len(selected_user_friends['items']) > 200:
 
-        putan_rating = putan_rating + 1.45
+        putan_rating = putan_rating + 0.95
         options = options + 1
     if 400 >= len(selected_user_friends['items']) > 300:
 
-        putan_rating = putan_rating + 1.95
+        putan_rating = putan_rating + 1.05
         options = options + 1
     if 500 >= len(selected_user_friends['items']) > 400:
 
-        putan_rating = putan_rating + 2.45
+        putan_rating = putan_rating + 1.15
         options = options + 1
     if 500 < len(selected_user_friends['items']):
 
-        putan_rating = putan_rating + 2.85
+        putan_rating = putan_rating + 1.25
         options = options + 1
 
 
@@ -134,20 +144,26 @@ def su_us(id):
             if 1 < len(selected_user_groups['items']) <= 3:
                 putan_rating = putan_rating + 0.65
                 options = options + 1
+
             if 3 < len(selected_user_groups['items']) <= 6:
-                putan_rating = putan_rating + 0.97
+                putan_rating = putan_rating + 0.87
                 options = options + 1
+
             if len(selected_user_groups['items']) > 6:
-                putan_rating = putan_rating + 1.13
+                putan_rating = putan_rating + 1.03
                 options = options + 1
+
+
 
         if unhashed_selected_user_groups in putan_groups_music:
             if 1 < len(selected_user_groups['items']) <= 4:
                 putan_rating = putan_rating + 0.7
                 options = options + 1
+
             if len(selected_user_groups['items']) > 4:
                 putan_rating = putan_rating + 1.01
                 options = options + 1
+
 
 
     for selected_user_stats in selected_user:
@@ -156,8 +172,7 @@ def su_us(id):
             options = options + 1
 
 
-    if putan_rating > 10:
-        putan_rating = putan_rating - putan_rating / 100 * 20
+
 
     print('.\n.\n.\n' + str(round(putan_rating * 10, 1)) + '% ----- normal putan value ~ 27%-51% \nvalidated options: ' + str(options) + ' out of 25 сircuits')
 
